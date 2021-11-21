9c490d4f69178426a51cfb724f35c2162df6d624
schimpfen <nathanschimpf@gmail.com>
2021-11-21 13:40:36 -0500

    Completed .bib scraping

--- keyword-scraper.py
+++ keyword-scraper.py
@@ -1,15 +1,10 @@
 from pybtex.database.input import bibtex
+import csv, os
+import utils as u
 
-def str_list(string: str, delim=','):
-    outlist = []
-    if string.count(delim) == 0: outlist.append(string)
-    for i in range(string.count(delim)):
-        part = string.partition(delim)
-        outlist.append(part[0])
-        string = part[-1]
-    return outlist
 
-file = "C:/Users/natha/Downloads/SpecShar.bib"
+
+file = "SpecShar.bib"
 papers_index = []
 fh = open(file, 'r')
 
@@ -19,31 +14,44 @@ bibfile = parser.parse_file(file)
 
 keys = bibfile.entries._keys
 
-solns, techs, apps = [], [], []
+solns, techs, apps, systems = [], [], [], []
 for key in keys:
     try:
         soln = bibfile.entries[key].fields['solntype']
-        soln = str_list(soln)
+        soln = u.str_list(soln)
         solns.extend(soln)
     except KeyError:
         pass
     try:
         tech = bibfile.entries[key].fields['technologies']
-        tech = str_list(tech)
+        tech = u.str_list(tech)
         techs.extend(tech)
     except KeyError:
         pass
     try:
         app = bibfile.entries[key].fields['app']
-        app = str_list(app)
+        app = u.str_list(app)
         apps.extend(app)
     except KeyError:
         pass
-solns, techs, apps = list(set(solns)), list(set(techs)), list(set(apps))
-print("Solns: {}\n\rTechs: {}\r\nApps: {}".format(solns, techs, apps))
-print("Solns{}| Tech{}| Apps{}\r\n{}".format(" "*24, " "*25, " "*25, "_"*90))
-while max([len(solns), len(techs), len(apps)]) > 0:
-    soln, tech, app = " "*29, " "*29, " "*29
+    try:
+        sys = bibfile.entries[key].fields['systems']
+        sys = u.str_list(sys)
+        systems.extend(sys)
+    except KeyError:
+        pass
+solns, techs, apps, systems = list(set(solns)), list(set(techs)), list(set(apps)), list(set(systems))
+
+# Save each category to CSV
+if not os.path.isdir("Tags"): os.mkdir("Tags")
+for name, arr in [('soln',solns), ('tech',tech), ('apps',apps), ('systems',systems)]:
+    u.writelist("Tags/{}".format(name), arr)
+
+
+# Print formatted table of keyword categories
+print("Apps{}| Tech{}| Solns{}| Systems{}\r\n{}".format(" "*25, " "*25, " "*24, " "*23, "_"*120))
+while max([len(solns), len(techs), len(apps), len(systems)]) > 0:
+    soln, tech, app, system = " "*29, " "*29, " "*29, " "*29
     if len(solns) > 0:
         x = solns.pop(0)
         soln = x + soln[len(x):]
@@ -53,5 +61,9 @@ while max([len(solns), len(techs), len(apps)]) > 0:
     if len(apps) > 0:
         x = apps.pop(0)
         app = x + app[len(x):]
-    print("{}| {}| {}".format(soln, tech, app))
+    if len(systems) > 0:
+        x = systems.pop(0)
+        system = x + system[len(x):]
+    print("{}| {}| {}| {}".format(app, tech, soln, system))
+
 
